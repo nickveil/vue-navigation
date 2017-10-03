df9c9bb3e50cd553e94a7288dfdc9de1781e6afd
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-03 11:52:02 -0400

    MVP buttons change,message change,remove active class from anchor

--- app/main.js
+++ app/main.js
@@ -2,10 +2,49 @@ var menu = new Vue({
 	el: '#menu',
 
 	data: {
+		homeActive : false,
+		projectsActive : false,
+		servicesActive : false,
+		contactActive : false,
 		message : 'Please click a menu item'
 
 	},
 	methods: {
+		makeActive: function( item, label ) {
+			console.log('makeActive');
+			console.log('makeActive - item ', item );
+
+			// Remove active class from anchor
+			this.homeActive = false;
+			this.projectsActive = false;
+			this.servicesActive = false;
+			this.contactActive = false;
+
+
+			// Add active class to the selected anchor(item)
+			switch( item ){
+				case'homeMenu':
+
+					this.homeActive=true;
+					break;
+					case'projectsMenu':
+
+					this.projectsActive=true;
+					break;
+					case'servicesMenu':
+
+					this.servicesActive=true;
+					break;
+					case'contactMenu':
+
+					this.contactActive=true;
+					break;
+			}
+
+
+			// Change the text of message
+			this.message = 'You chose ' + label;
+		}
 
 	}
 });
