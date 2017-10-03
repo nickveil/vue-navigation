e5dc939171e878fba524c57939845275730a28ca
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-03 14:18:34 -0400

    Refactoring html and vue

--- app/main.js
+++ app/main.js
@@ -1,49 +1,12 @@
 var menu = new Vue({
-	el: '#menu',
+	el : '#menu',
 
-	data: {
-		homeActive : false,
-		projectsActive : false,
-		servicesActive : false,
-		contactActive : false,
-		message : 'Please click a menu item'
-
+	data : {
+		active: false
 	},
-	methods: {
-		makeActive: function( item, label ) {
-			console.log('makeActive');
-			console.log('makeActive - item ', item );
-
-			// Remove active class from anchor
-			this.homeActive = false;
-			this.projectsActive = false;
-			this.servicesActive = false;
-			this.contactActive = false;
-
-
-			// Add active class to the selected anchor(item)
-			switch( item ){
-				case'homeMenu':
-
-					this.homeActive=true;
-					break;
-					case'projectsMenu':
-
-					this.projectsActive=true;
-					break;
-					case'servicesMenu':
-
-					this.servicesActive=true;
-					break;
-					case'contactMenu':
-
-					this.contactActive=true;
-					break;
-			}
-
-
-			// Change the text of message
-			this.message = 'You chose ' + label;
+	methods : {
+		makeActive: function( item) {
+			this.active = item;
 		}
 
 	}
